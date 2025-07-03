21b9db36dded7a0af647d7013f65e55629f65c19
Danylo Honcharov <honcharov.danylo@gmail.com>
2025-07-03 18:33:04 -0400

    first commit

---
+++ eval.py
@@ -0,0 +1,99 @@
+from transformers import AutoModelForCausalLM
+from peft import PeftModel
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+import json
+import os
+import logging
+import gzip
+import simphile
+import spacy
+
+# device = "cpu" # can be "cpu" or "cuda
+# inference on cuda takes too much memory
+
+with open("config.json", 'r') as f:
+    config = json.load(f)
+
+base = AutoModelForCausalLM.from_pretrained(config["model_dir"], device_map="auto")
+
+model = PeftModel.from_pretrained(base, config["finetuned_path"]).to("cuda")
+
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+
+tokenizer = AutoTokenizer.from_pretrained(config["finetuned_path"], use_fast=True)
+
+test_prompt_style = """Below is an instruction that describes a task, paired with an input that provides further context.
+Write a response that appropriately completes the request.
+
+### Instruction:
+You are a scientist with advanced knowledge in philosophy and social sciences.
+Please, continue for the following text.
+
+### Text:
+{}
+
+### Response:
+"""
+
+logging.info("Loading dataset:")
+
+
+if config['load_cleaned']:
+    inputs = []
+    for file in os.listdir(config["data_path"]):
+        with gzip.open("{}/{}".format(config["data_path"], file), "rt") as f:
+            subsamples = json.load(f)
+            inputs.extend(list(subsamples.values()))
+else:
+    inputs = []
+    for file in os.listdir(config["data_path"]):
+        with open("{}/{}".format(config["data_path"], file)) as f:
+            inputs.append(f.read())
+    del inputs[6326] # broken file
+
+
+inputs_in = [test_prompt_style.format(x[:int(len(x)/2)]) + tokenizer.eos_token for x in inputs]
+inputs_out = [x[int(len(x)/2):] for x in inputs]
+
+
+inputs_formatted = tokenizer(
+    inputs_in,
+    return_tensors="pt"
+).to("cuda")
+
+outputs_orig = base.generate(
+    input_ids=inputs_formatted.input_ids,
+    attention_mask=inputs_formatted.attention_mask,
+    max_new_tokens=1200,
+    eos_token_id=tokenizer.eos_token_id,
+    use_cache=True,
+    batch_size=config["batch_size"],)
+
+outputs = model.generate(
+    input_ids=inputs_formatted.input_ids,
+    attention_mask=inputs_formatted.attention_mask,
+    max_new_tokens=1200,
+    eos_token_id=tokenizer.eos_token_id,
+    use_cache=True,
+    batch_size=config["batch_size"],
+)
+
+responses_orig = tokenizer.batch_decode(outputs_orig, skip_special_tokens=True)
+
+responses = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+
+nlp = spacy.load("en_core_web_sm")
+
+nlp_input_out = [nlp(x) for x in inputs_out]
+
+results = dict()
+results["orig"] = dict()
+results["orig"]["jaccard"] = [simphile.jaccard_similarity(x, inputs_out[i]) for i,x in enumerate(responses_orig)]
+results["orig"]["compression"] = [simphile.compression_similarity(x, inputs_out[i]) for i,x in enumerate(responses_orig)]
+results["orig"]["spacy_sim"] = [nlp(x).similarity(nlp_input_out[i]) for i,x in enumerate(responses_orig)]
+
+
+results["finetuned"] = dict()
+results["finetuned"]["jaccard"] = [simphile.jaccard_similarity(x, inputs_out[i]) for i,x in enumerate(responses)]
+results["finetuned"]["compression"] = [simphile.compression_similarity(x, inputs_out[i]) for i,x in enumerate(responses)]
+results["finetuned"]["spacy_sim"] = [nlp(x).similarity(nlp_input_out[i]) for i,x in enumerate(responses)]
