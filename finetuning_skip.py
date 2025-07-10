7e85884bb6a82944aae379f48a744d6e6be850a7
Danylo Honcharov <honcharov.danylo@gmail.com>
2025-07-10 11:54:17 -0400

    fixing eval

--- finetuning_skip.py
+++ finetuning_skip.py
@@ -282,7 +282,7 @@ dataset = ds.map(
 eval_dataset_mapped = eval_ds.map(
     formatting_prompts_func,
     batched=True,
-).map(truncate_long_prompts, batched=True,)
+).map(truncate_long_prompts)
 
 
 logging.info("Datasets are formatted.")
