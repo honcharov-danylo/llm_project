d4d1699af062938ab38056532f8ff789d3dfa9d3
josk0 <53160902+josk0@users.noreply.github.com>
2025-07-22 22:46:23 -0400

    added download_file and refactored

--- b2_uploader/src/b2_uploader.py
+++ b2_uploader/src/b2_uploader.py
@@ -1,10 +1,13 @@
 import os
+import sys
+from dotenv import load_dotenv
 from b2sdk.v2 import (
     B2Api, InMemoryAccountInfo, AuthInfoCache, ScanPoliciesManager,
     parse_folder, Synchronizer, SyncReport, CompareVersionMode,
     NewerFileSyncMode
 )
 import time
+
 
 class UploaderBase:
     def __init__(self):
@@ -13,43 +16,93 @@ class UploaderBase:
     def upload_file(self, file_path, destination_path):
         pass
 
+    def download_file(self, bucket_path, dest_directory):
+        pass
+
+
 class B2Uploader(UploaderBase):
-    def __init__(self):
+    def __init__(self, b2_bucket_name=None):
         super().__init__()
+        self.b2_bucket_name = b2_bucket_name
+        
+        # Load API Key and bucket name
+        load_dotenv()
         self.b2_api_key_id = os.getenv("B2_APPLICATION_KEY_ID")
         self.b2_api_key = os.getenv("B2_APPLICATION_KEY")
-        self.b2_bucket_name = os.getenv("B2_BUCKET_NAME")
+
+        # Set b2_bucket_name to value from env if not provided
+        if b2_bucket_name is None:
+            b2_bucket_name = os.getenv("B2_BUCKET_NAME")
 
         # Validate environment variables
         if not all([self.b2_api_key_id, self.b2_api_key, self.b2_bucket_name]):
             raise ValueError(
                 "Missing required environment variables: B2_APPLICATION_KEY_ID, B2_APPLICATION_KEY, or B2_BUCKET_NAME")
 
-        # Assert that b2_bucket_name is a string
-        assert isinstance(self.b2_bucket_name, str)
+        # Check that b2_bucket_name is a string, set path to bucket (required for some API functions)
+        if not isinstance(self.b2_bucket_name, str):
+            raise TypeError("B2 bucket name must be a string")
         self.b2_bucket_name_path = 'b2://' + self.b2_bucket_name
 
+        try:
+            # Initialize the B2 API
+            info = InMemoryAccountInfo()
+            self.b2_api = B2Api(info, cache=AuthInfoCache(info)) # type: ignore
+            self.b2_api.authorize_account("production", self.b2_api_key_id, self.b2_api_key)
+        
+        except Exception as e:
+            print(f"B2 API initialization failed: {e}")
+            raise(e)
 
 
     def upload_file(self, file_path, dest_directory):
         """
         Upload a file to B2.
         :param file_path: Path to the file to upload.
-        :param dest_directory: Destination directory.
+        :param dest_directory: Destination directory in B2 bucket.
         """
-        self.source_path = os.path.abspath(file_path)
-        if not os.path.exists(self.source_path):
-            raise ValueError(f"Source path does not exist: {self.source_path}")
+        source_path = os.path.abspath(file_path)
+        if not os.path.exists(source_path):
+            raise ValueError(f"Source path does not exist: {source_path}")
+        
         try:
-            # Initialize the B2 API
-            info = InMemoryAccountInfo()
-            b2_api = B2Api(info, cache=AuthInfoCache(info))
-            b2_api.authorize_account("production", self.b2_api_key_id, self.b2_api_key)
-
             # Parse folders
-            local_folder = parse_folder(self.source_path, b2_api)
-            bucket_folder = parse_folder(f"{self.b2_bucket_name_path}/{dest_directory}", b2_api)
+            local_folder = parse_folder(source_path, self.b2_api)
+            bucket_folder = parse_folder(f"{self.b2_bucket_name_path}/{dest_directory}", self.b2_api)
 
+        except Exception as e:
+            raise e
+        
+        self._push(local_folder, bucket_folder)
+
+
+    def download_file(self, bucket_path, dest_directory):
+        """
+        Download a file or directory from B2.
+        :param bucket_path: Path in bucket to download.
+        :param dest_directory: Destination directory local.
+        """
+        dest_path = os.path.abspath(dest_directory)
+        if not os.path.exists(dest_path):
+            raise ValueError(f"Destination path does not exist: {dest_path}")
+
+        try:
+            # Parse folders
+            local_folder = parse_folder(dest_path, self.b2_api)
+            bucket_folder = parse_folder(f"{self.b2_bucket_name_path}/{bucket_path}", self.b2_api)
+        except Exception as e:
+                raise e
+        
+        self._push(bucket_folder, local_folder)
+
+
+    def _push(self, parsed_source_path, parsed_dest_path):
+        """
+        Sync files from source to destination.
+        :param parsed_source_path: Parsed source path.
+        :param parsed_dest_path: Parsed destination path.
+        """
+        try:
             policies_manager = ScanPoliciesManager(exclude_all_symlinks=True)
 
             synchronizer = Synchronizer(
@@ -63,15 +116,12 @@ class B2Uploader(UploaderBase):
             )
             with SyncReport(sys.stdout, no_progress=False) as reporter:  # type: ignore
                 synchronizer.sync_folders(
-                    # source_folder=bucket_folder,
-                    # dest_folder=local_folder,
-                    source_folder=local_folder,
-                    dest_folder=bucket_folder,
+                    source_folder=parsed_source_path,
+                    dest_folder=parsed_dest_path,
                     now_millis=int(round(time.time() * 1000)),
                     reporter=reporter,
                 )
 
         except Exception as e:
             raise e
-
-
+        pass
