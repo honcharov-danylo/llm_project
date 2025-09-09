330decabc894bdd8a2ad649bde75ab1eecdc7b9f
josk0 <53160902+josk0@users.noreply.github.com>
2025-09-09 19:13:47 +0200

    try to use other conda env

--- llm_scripts/conda_wrapper_fine.sh
+++ llm_scripts/conda_wrapper_fine.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-eval "$(/home/$(whoami)/anaconda3/bin/conda shell.bash hook)"
+# eval "$(/home/$(whoami)/anaconda3/bin/conda shell.bash hook)"
+eval "$(/home/dhonchar/anaconda3/bin/conda shell.bash hook)"
 
-source "/home/$(whoami)/.bashrc"
+# source "/home/$(whoami)/.bashrc"
+/home/dhonchar/.bashrc
 export WANDB_PROJECT=finetune-llm
 export CUDA_LAUNCH_BLOCKING=1 # for debugging only, for more accurate stacktracing
 
